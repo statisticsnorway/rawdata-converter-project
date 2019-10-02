e850a148838bb74fb75fd35d7a2589b18253e5d8
Kenneth Leine Schulstad <ks@udp.no>
2019-10-02 12:23:41 +0200

    Echo commands

--- checkout.sh
+++ checkout.sh
@@ -7,6 +7,7 @@ declare -a repos=(
 
 for repo in "${repos[@]}"
 do
+    echo git clone git@github.com:statisticsnorway/${repo}.git
 	git clone git@github.com:statisticsnorway/${repo}.git
 done
 
--- pull.sh
+++ pull.sh
@@ -7,6 +7,7 @@ declare -a repos=(
 
 for repo in "${repos[@]}"
 do
+    echo git -C ${repo}/ pull --rebase
 	git -C ${repo}/ pull --rebase
 done
 
